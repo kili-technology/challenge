b44970c7a2e4067a8e3649b761db83ddbf2a079a
Pierre Leveau <pierre.leveau@kili-technology.com>
2022-02-11 10:58:35 +0100

    update with "prod" environment

--- kili_to_hf_dataset/entrypoint.sh
+++ kili_to_hf_dataset/entrypoint.sh
@@ -2,10 +2,10 @@
 case $1 in 
 
     kili2hfds)
-    PYTHONPATH=$(pwd) python cmd/kili_to_local.py --local-path $2
-    bash cmd/upload_to_gcs.sh $2
-    datasets-cli test litter-challenge-test/ --save_infos --all_configs
-    cd litter-challenge-test/ && git add dataset_infos.json && git commit -m "Updated from Kili" && git push
+    PYTHONPATH=$(pwd) python cmd/kili_to_local.py --local-path $2 && \
+    bash cmd/upload_to_gcs.sh $2 && \
+    datasets-cli test plastic_in_river/ --save_infos --all_configs && \
+    cd plastic_in_river/ && git add dataset_infos.json && git commit -m "Updated from Kili" && git push
     ;;
 
     *)
