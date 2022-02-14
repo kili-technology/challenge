6e888efbfdfb1fa15063c8a6e15a093c0d70a98a
Pierre Leveau <pierre.leveau@kili-technology.com>
2022-02-14 17:21:32 +0100

    add 4th class

--- kili_to_hf_dataset/cmd/kili_to_local.py
+++ kili_to_hf_dataset/cmd/kili_to_local.py
@@ -5,13 +5,13 @@ from kili2hfds.downloader import Downloader #, Uploader
 @click.command()
 @click.option('--local-path', help='Path of unzipped folder from https://bit.ly/3J8wGkU')
 def main(local_path):
-    
+
     dl = Downloader()
-    
+
     kili_project_id = "ckzdzhh260ec00mub7gqjfetz"
 
     # TODO: label names should be fetched from Kili's JSON interface
-    dl.download_and_save_by_split(kili_project_id, local_path, ["PLASTIC_BAG", "PLASTIC_BOTTLE", "OTHER_PLASTIC_WASTE"]) 
+    dl.download_and_save_by_split(kili_project_id, local_path, ["PLASTIC_BAG", "PLASTIC_BOTTLE", "OTHER_PLASTIC_WASTE", "NOT_PLASTIC_WASTE"])
 
 if __name__ == '__main__':
     main()
