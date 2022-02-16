685a262e61b628b0bfb0a5d8709876528db455e5
Pierre Leveau <pierre.leveau@kili-technology.com>
2022-02-16 11:49:41 +0100

    /tmp/kili2hfds/plastic_in_river as the loc path

--- kili_to_hf_dataset/entrypoint.sh
+++ kili_to_hf_dataset/entrypoint.sh
@@ -2,9 +2,11 @@
 case $1 in
 
     kili2hfds)
-    PYTHONPATH=$(pwd) python cmd/kili_to_local.py --local-path $2 && \
+    LOCAL_PATH=/tmp/kili2hfds/plastic_in_river
+    mkdir -p $LOCAL_PATH && \
+    PYTHONPATH=$(pwd) python cmd/kili_to_local.py --local-path $LOCAL_PATH && \
     bumpversion --current-version $(cat VERSION) minor VERSION plastic_in_river/plastic_in_river.py && \
-    bash cmd/upload_to_gcs.sh $2 && \
+    bash cmd/upload_to_gcs.sh $LOCAL_PATH && \
     datasets-cli test plastic_in_river/ --save_infos --all_configs && \
     cd plastic_in_river/ && git submodule update --remote && git add dataset_infos.json && git commit -m "Updated from Kili: v$(cat VERSION)" && git push HEAD:main && cd .. && \
     git add VERSION && git commit -m "Dataset updated from Kili: v$(cat VERSION)"
