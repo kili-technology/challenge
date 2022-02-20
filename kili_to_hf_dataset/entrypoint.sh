c510023fb753f08ffca74fbcb52f36a3230d98de
Pierre Leveau <pierre.leveau@kili-technology.com>
2022-02-20 12:51:06 +0100

    push VERSION as well

--- kili_to_hf_dataset/entrypoint.sh
+++ kili_to_hf_dataset/entrypoint.sh
@@ -10,7 +10,7 @@ case $1 in
     bash cmd/upload_to_gcs.sh $LOCAL_PATH && \
     datasets-cli test plastic_in_river/ --save_infos --all_configs && \
     cd plastic_in_river/ && git submodule update --remote && git add dataset_infos.json plastic_in_river.py && git commit -m "Updated from Kili: v$(cat ../VERSION)" && git push origin HEAD:main && cd .. && \
-    git add VERSION && git commit -m "Dataset updated from Kili: v$(cat VERSION)"
+    git add VERSION && git commit -m "Dataset updated from Kili: v$(cat VERSION)" && git push
     ;;
 
     *)
